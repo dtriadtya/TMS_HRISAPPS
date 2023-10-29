1ae8bc32508f495c3d5d89cd8af645c79a2dcdfa
dtriadtya <triadtyax1@gmail.com>
2023-10-29 21:31:12 +0700

    FRONTEND

---
+++ android/app/src/main/kotlin/com/example/hris_tms/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hris_tms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/globals.dart
@@ -0,0 +1,12 @@
+//nabila , 23 oktober
+library globals;
+
+String urlAPI = 'http://192.168.197.250:8000/api';
+
+int userId = 6;
+
+String locationNow = '';
+
+double latitudeNow = 0.0;
+
+double longtitudeNow = 0.0;
