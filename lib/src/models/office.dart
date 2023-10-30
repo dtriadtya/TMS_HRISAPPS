0261b9fb15c279cb2865c55385a47e8f2e238e1a
Nabila Latifah Azzahra <138288835+Nabilaltfh@users.noreply.github.com>
2023-10-30 11:18:55 +0700

    Update office.dart

--- lib/src/models/office.dart
+++ lib/src/models/office.dart
@@ -1,1 +1,12 @@
+//Nabila Latifah Azzahra, 23 Oktober 2023, 10.07 WIB (START)
+class Office {
+  final String name;
+  final String loc;
 
+  Office({required this.name, required this.loc});
+
+  factory Office.fromJson(Map<String, dynamic> json) {
+    return Office(name: json['name'], loc: json['loc']);
+  }
+}
+////Nabila Latifah Azzahra, 23 Oktober 2023, 15.30 WIB (END)
