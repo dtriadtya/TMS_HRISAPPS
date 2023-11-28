d1178c870ca664bc033ccd165d8d385298ce681f
Ardiansyah Tria Sati <122155985+katssss21@users.noreply.github.com>
2023-11-28 20:02:17 +0700

    ADD presensi_screen.dart

--- lib/src/screens/presensi/presensi_screen.dart
+++ lib/src/screens/presensi/presensi_screen.dart
@@ -1,1 +1,22 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+//Ardiansyah Tria Sati, 01 November 2023, 12.09 WIB (START)
+import 'package:flutter/material.dart';
+import 'package:tmshub/src/widgets/presensi_widgets/live_attend1.dart';
+import 'package:tmshub/src/widgets/top_navigation.dart';
 
+class PresensiScreen extends StatelessWidget {
+  const PresensiScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          children: [
+            TopNavigation(title: "presensi"),
+            LiveAttendPage1()
+          ],
+        ),
+      );
+  }
+}
+//Ardiansyah Tria Sati, 01 November 2023, 14.40 WIB (END)
