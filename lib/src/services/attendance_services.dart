ac3eb19da371fd7a330c6e0f1fb73b1adeaf4eac
Ardiansyah Tria Sati <122155985+katssss21@users.noreply.github.com>
2023-11-28 20:20:11 +0700

    Add attendance_services.dart

--- lib/src/services/attendance_services.dart
+++ lib/src/services/attendance_services.dart
@@ -1,1 +1,17 @@
+//Ardiansyah Tria Sati, 01 November 2023, 19.45 WIB (START)
+import 'dart:convert';
 
+import 'package:tmshub/src/models/office.dart';
+import 'package:tmshub/src/utils/api_utils.dart';
+
+Future<List<Office>> fetchDataOffice() async {
+  final jsonString = await loadOfficeData();
+  final List<Map<String, dynamic>> officeDataList =
+      (json.decode(jsonString)['data'] as List<dynamic>)
+          .cast<Map<String, dynamic>>();
+  final List<Office> officeList = officeDataList.map((officeData) {
+    return Office.fromJson(officeData);
+  }).toList();
+  return officeList;
+}
+//Ardiansyah Tria Sati, 01 November 2023, 21.58 WIB (END)
