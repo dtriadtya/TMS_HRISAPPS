fd7ce248ff81940d98cb544a6e65ccf91e7710db
Ardiansyah Tria Sati <122155985+katssss21@users.noreply.github.com>
2023-11-28 20:13:53 +0700

    Add top_navigation.dart

--- lib/src/widgets/top_navigation.dart
+++ lib/src/widgets/top_navigation.dart
@@ -1,1 +1,91 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last, sized_box_for_whitespace
+//Ardiansyah Tria Sati, 05 November 2023, 07.28 WIB (START)
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
+class TopNavigation extends StatelessWidget {
+  final String title;
+
+  const TopNavigation({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 22),
+      child: Container(
+        padding: EdgeInsets.all(5),
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          children: [
+            Container(
+              width: 53,
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop(true);
+                  },
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 5, bottom: 5),
+                      child: Icon(
+                        Icons.arrow_back_outlined,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: HexColor("#E5F1F8"),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 0,
+                    blurRadius: 1,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(
+                  top: 8,
+                  bottom: 8,
+                ),
+                decoration: BoxDecoration(
+                  color: HexColor("#E5F1F8"),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 0,
+                      blurRadius: 1,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  title.toUpperCase(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Montserrat",
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+//Ardiansyah Tria Sati, 05 November 2023, 13.09 WIB (END)
